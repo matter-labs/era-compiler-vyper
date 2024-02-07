f264b3c1ce9b4d5e793c42dc9864d7e50163de80
pcheremu <pavel.cheremushkin@gmail.com>
2024-02-07 18:49:53 +0100

    test: refactoring

--- src/tests/cli-tests/src/helper.ts
+++ src/tests/cli-tests/src/helper.ts
@@ -4,18 +4,10 @@ import { spawnSync } from "child_process";
 
 export function executeCommand(command: string, args: string[]) {
   const result = spawnSync(command, args, { encoding: 'utf-8', shell: true, stdio: 'pipe' });
-  
-  if (result.stderr) {
-    return {
-      exitCode: result.status || 1, error: result.error,
-      output: result.stderr.trim()
-    };
-  } else {
-    return {
-      exitCode: result.status || 0,
+  return {
+      exitCode: result.status,
       output: result.stdout.trim() || result.stderr.trim()
-    };
-  }
+  };
 }
 
 export const isDestinationExist = (destination: string): boolean  => {
