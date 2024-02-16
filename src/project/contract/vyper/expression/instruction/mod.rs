778b911623484c9c8b2e01d1f304121528c79da6
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-02-16 16:26:44 +0100

    Replace memcpy with memmove

--- src/project/contract/vyper/expression/instruction/mod.rs
+++ src/project/contract/vyper/expression/instruction/mod.rs
@@ -1019,7 +1019,7 @@ impl Instruction {
                 );
 
                 context.build_memcpy(
-                    context.intrinsics().memory_copy,
+                    context.intrinsics().memory_move,
                     destination,
                     source,
                     arguments[2].into_int_value(),
