8fc8c76e4592f7b16619ab9b76a8f38521f54e4b
pcheremu <51121511+pcheremu@users.noreply.github.com>
2024-04-22 16:24:59 +0200

    test: initial commit with tests (#42)
    Co-authored-by: Anton Baliasnikov <aba@matterlabs.dev>

--- cli-tests/src/helper.ts
+++ cli-tests/src/helper.ts
@@ -1,6 +1,10 @@
 import * as fs from 'fs';
 import { spawnSync } from "child_process";
+import * as tmp from 'tmp';
+import { paths } from './entities';
+import * as path from 'path';
 
+tmp.setGracefulCleanup();
 
 export function executeCommand(command: string, args: string[]) {
   const result = spawnSync(command, args, { encoding: 'utf-8', shell: true, stdio: 'pipe' });
@@ -18,4 +22,19 @@ export const isFileEmpty = (file: string): boolean  => {
     if (isDestinationExist(file)) {
         return (fs.readFileSync(file).length === 0);
     } 
+};
+
+export const createTmpDirectory = (name = 'tmp-XXXXXX'): tmp.DirResult => {
+    if (!fs.existsSync(paths.pathToOutputDir)) {
+        fs.mkdirSync(paths.pathToOutputDir, { recursive: true });
+    }
+    return tmp.dirSync({ template: name, tmpdir: paths.pathToOutputDir, unsafeCleanup: true });
+};
+
+export const pathToVyBinOutputFile = (destination: string): string  => {
+    return path.join(destination, paths.contractVyFilename + paths.binExtension);
+};
+
+export const pathToVyAsmOutputFile = (destination: string): string  => {
+    return path.join(destination, paths.contractVyFilename + paths.asmExtension);
 };
--- cli-tests/tests/common.test.ts
+++ cli-tests/tests/common.test.ts
@@ -6,12 +6,13 @@ describe("Common tests", () => {
     const zkvyperCommand = 'zkvyper';
     const vyperCommand = 'vyper';
 
+    //id1936
     describe("Run zkvyper without any options", () => {
         const args = [''];
         const result = executeCommand(zkvyperCommand, args);
 
         it("Info with help is presented", () => {
-            expect(result.output).toMatch(/(the following arguments are required: input_files)/i);
+            expect(result.output).toMatch(/(the following arguments are required: input_files|Disable the system request memoization)/i);
         });
 
         it("Exit code = 1", () => {
@@ -23,8 +24,30 @@ describe("Common tests", () => {
         });
     });
 
+    //id1978
+    describe("Default run with a contract only", () => {
+        const args = [`"${paths.pathToBasicVyContract}"`]; // issue on windows
+        const result = executeCommand(zkvyperCommand, args);
 
-    //
+        it("Exit code = 0", () => {
+            expect(result.exitCode).toBe(0);
+        });
+
+        it("Info with a tx hash is presented", () => {
+            expect(result.output).toMatch(/(0x)/i);
+        });
+
+        it("No 'Error'/'Warning'/'Fail' in the output", () => {
+            expect(result.output).not.toMatch(/([Ee]rror|[Ww]arning|[Ff]ail)/i);
+        });
+
+        it("vyper exit code == zkvyper exit code", () => {
+            const vyperResult = executeCommand(vyperCommand, args);
+            expect(vyperResult.exitCode).toBe(result.exitCode);
+        });
+    });
+
+    //id1983
     describe("Default run a command from the help", () => {
         const args = [`"${paths.pathToBasicVyContract}"`, `-o`, `"${paths.pathToOutputDir}"`]; // issue on windows
         const result = executeCommand(zkvyperCommand, args); 
