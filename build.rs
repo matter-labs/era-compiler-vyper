1b36aa2988bc7236c51392982761135c03063e0a
Anton Baliasnikov <aba@matterlabs.dev>
2024-05-08 11:38:30 +0300

    build: proper crt builtins linkage for aarch64-unknown-linux-musl

---
+++ build.rs
@@ -0,0 +1,10 @@
+//!
+//! The default build script for `zkvyper`.
+//!
+
+///
+/// The default build script for `zkvyper`.
+///
+/// Is required for `rust-link-lib` flags to work.
+///
+fn main() {}
