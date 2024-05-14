be50f7e2f7d95e7e44e31b409f6f434a6c57da19
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-05-14 14:50:32 +0200

    fix: rename the system mode

--- src/project/contract/vyper/mod.rs
+++ src/project/contract/vyper/mod.rs
@@ -322,7 +322,7 @@ impl EraVMDependency for DependencyData {
         _contract: Self,
         _name: &str,
         _optimizer_settings: era_compiler_llvm_context::OptimizerSettings,
-        _is_system_mode: bool,
+        _enable_eravm_extensions: bool,
         _include_metadata_hash: bool,
         _debug_config: Option<era_compiler_llvm_context::DebugConfig>,
     ) -> anyhow::Result<String> {
